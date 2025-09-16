3dcb98488837055a6b8408a644e4ce71cd77121a
Venus <venuskh.tagline@gmail.com>
2025-09-16 09:39:15 +0530

    Generate the background email and periodic tasks mails using celery and celery Base

--- Backend/celery.py
+++ Backend/celery.py
@@ -11,8 +11,11 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     "send-daily-pending-reminders": {
-        "task": "notifications.tasks.send_daily_pending_reminders",  # path to your task
-        "schedule": crontab(minute="*/3600"),  # every 1 minute (for testing)
-        # For daily at 9 AM: crontab(hour=9, minute=0)
+        "task": "notifications.tasks.send_pending_task_reminders",
+        "schedule": crontab(hour=9, minute=0),  # every day at 9:00 AM
+    },
+    "send-daily-inprogress-reminders": {
+        "task": "notifications.tasks.send_inprogress_task_reminders",
+        "schedule": crontab(hour=16, minute=0),  # every day at 5:00 PM
     },
 }
--- notifications/tasks.py
+++ notifications/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 from task.models import Task
+from task.enums import StatusChoice
 
 @shared_task
 def send_task_assigned_email(task_id, assignee_email):
@@ -26,14 +27,28 @@ def send_task_updated_email(task_id, assignee_email):
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [assignee_email],fail_silently=False)
 
 @shared_task
-def send_daily_pending_reminders():
+def send_pending_task_reminders():
     users_with_tasks = {}
-    pending_tasks = Task.objects.filter(status="PENDING",is_archived=False)
+    pending_tasks = Task.objects.filter(status=StatusChoice.PENDING,is_archived=False)
     for task in pending_tasks:
         if task.assigned_to:
             users_with_tasks.setdefault(task.assigned_to.email, []).append(task.title)
 
     for email, tasks in users_with_tasks.items():
-        subject = "Daily Pending Task Reminder"
+        subject = "Reminder: Pending Tasks"
         message = "You still have these pending tasks:\n" + "\n".join(tasks)
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email],fail_silently=False)
+
+
+@shared_task
+def send_inprogress_task_reminders():
+    users_with_tasks = {}
+    in_progress_tasks = Task.objects.filter(status=StatusChoice.IN_PROGRESS,is_archived=False)
+    for task in in_progress_tasks:
+        if task.assigned_to:
+            users_with_tasks.setdefault(task.assigned_to.email, []).append(task.title)
+
+    for email, tasks in users_with_tasks.items():
+        subject = "Reminder: Share Updates for In-Progress Tasks"
+        message =  "You are working on these tasks. Please share your progress updates:\n"+ "\n".join(tasks)
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email],fail_silently=False)
